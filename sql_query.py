43e17f4153c8fbdb7613ffc80076018fc41e9b68
spring01 <lihc2012@gmail.com>
2017-09-16 10:19:36 -0400

    sql_query.py

---
+++ sql_query.py
@@ -0,0 +1,19 @@
+
+import psycopg2
+from information import CONNECT
+
+
+
+if __name__ == '__main__':
+    db_conn = psycopg2.connect(CONNECT)
+    table_list = ['genes', 'gtex_donor', 'gtex_gene_expression_processed', 'gtex_gene_model',
+        'gtex_sample', 'gtex_sample_expression', 'gtex_tissue', 'tcga', 'toxicogenomics_chemicals', 'toxicogenomics_diseases']
+
+    cur = db_conn.cursor()
+    cur.execute("select * from genes")
+    print cur.fetchall()
+
+
+
+
+
